3fbd5183c5474578c5c584734a686056b75cca59
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-17 00:47:48 +0100

    issue done. closes #2.

---
+++ Exceptions/src/com/drronidz/Example.java
@@ -0,0 +1,28 @@
+package com.drronidz;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 5/17/2021 , 
+    CREATED ON : 12:37 AM
+*/
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+    }
+
+    private static int divide() {
+        int x = getInt();
+        int y = getInt();
+        System.out.println("x is " + x +", y is " + y);
+        return x / y;
+    }
+
+    private static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an Integer");
+        return scanner.nextInt();
+    }
+}
