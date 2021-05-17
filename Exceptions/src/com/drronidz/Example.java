397b7808ba7e2601f637bded444636fdf0dcc46f
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-18 00:12:19 +0100

    issue done. closes #4

--- Exceptions/src/com/drronidz/Example.java
+++ Exceptions/src/com/drronidz/Example.java
@@ -12,24 +12,27 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
         int x,y;
-        try {
+//        try {
             x = getInt();
             y = getInt();
-        } catch (NoSuchElementException e) {
-            throw  new ArithmeticException("No suitable input");
-        }
-        System.out.println("x is " + x +", y is " + y);
-        try {
+            System.out.println("x is " + x +", y is " + y);
             return x / y;
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("Attempt to divide by Zéro");
-        }
+//        } catch (NoSuchElementException  e) {
+//            throw  new ArithmeticException("No suitable input");
+//        } catch (ArithmeticException e) {
+//            throw new ArithmeticException("Attempt to divide by Zéro");
+//        }
     }
 
     private static int getInt() {
