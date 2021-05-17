5771a80add48bb30912036c7f77b4216937ae740
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-17 23:48:01 +0100

    issue done. closes #3

--- Exceptions/src/com/drronidz/Example.java
+++ Exceptions/src/com/drronidz/Example.java
@@ -5,24 +5,45 @@ package com.drronidz;/*
     CREATED ON : 12:37 AM
 */
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
+
     public static void main(String[] args) {
         int result = divide();
         System.out.println(result);
     }
 
     private static int divide() {
-        int x = getInt();
-        int y = getInt();
+        int x,y;
+        try {
+            x = getInt();
+            y = getInt();
+        } catch (NoSuchElementException e) {
+            throw  new ArithmeticException("No suitable input");
+        }
         System.out.println("x is " + x +", y is " + y);
-        return x / y;
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Attempt to divide by Zéro");
+        }
     }
 
     private static int getInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter an Integer");
-        return scanner.nextInt();
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                // go round again. Read past the end of line in the input first.
+                scanner.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9");
+            }
+        }
+
     }
 }
