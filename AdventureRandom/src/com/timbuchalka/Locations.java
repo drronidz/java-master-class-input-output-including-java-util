43e6579039102a579b020b1922d31e1890c148e3
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-21 23:27:33 +0100

    issue done. closes #19.

--- AdventureRandom/src/com/timbuchalka/Locations.java
+++ AdventureRandom/src/com/timbuchalka/Locations.java
@@ -75,26 +75,31 @@ public class Locations implements Map<Integer, Location> {
         } catch (IOException e) {
             System.out.println("IOException in static initializer: " + e.getMessage());
         }
-  /*      try(ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))) {
-            boolean eof = false;
-            while(!eof) {
-                try {
-                    Location location = (Location) locFile.readObject();
-                    System.out.println("Read location " + location.getLocationID() + " : " + location.getDescription());
-                    System.out.println("Found " + location.getExits().size() + " exits");
 
-                    locations.put(location.getLocationID(), location);
-                } catch(EOFException e) {
-                    eof = true;
-                }
-            }
-        } catch(IOException io) {
-            System.out.println("IO Exception" + io.getMessage());
-        } catch(ClassNotFoundException e) {
-            System.out.println("ClassNotFoundException " + e.getMessage());
-        }*/
 
     }
+
+    public Location getLocation(int locationID) throws IOException {
+        IndexRecord record = index.get(locationID);
+        randomAccessFile.seek(record.getStartByte());
+        int id  = randomAccessFile.readInt();
+        String description = randomAccessFile.readUTF();
+        String exits = randomAccessFile.readUTF();
+        String [] exitPart = exits.split(",");
+        Location location = new Location(locationID, description, null);
+
+        if(locationID != 0) {
+            for(int i=0; i<exitPart.length; i++) {
+                System.out.println("exitPart = " + exitPart[i]);
+                System.out.println("exitPart[+1]" + exitPart[i+1]);
+                String direction = exitPart [i];
+                int destination = Integer.parseInt(exitPart[++i]);
+                location.addExit(direction, destination);
+            }
+        }
+        return location;
+    }
+
     @Override
     public int size() {
         return locations.size();
@@ -154,5 +159,9 @@ public class Locations implements Map<Integer, Location> {
     @Override
     public Set<Entry<Integer, Location>> entrySet() {
         return locations.entrySet();
+    }
+
+    public void close() throws IOException {
+        randomAccessFile.close();
     }
 }
