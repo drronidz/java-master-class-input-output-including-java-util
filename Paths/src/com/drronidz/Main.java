9bb99257279d3cc4fab1adb7c3795b0703937cb1
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-26 13:48:58 +0100

    issue done. closes #32

--- Paths/src/com/drronidz/Main.java
+++ Paths/src/com/drronidz/Main.java
@@ -7,20 +7,32 @@ package com.drronidz;/*
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class Main {
+
     public static void main(String[] args) {
-
-
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Paths","Examples","file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Paths","Examples","file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+
+
+            Path fileToDelete = FileSystems.getDefault().getPath("Paths","Examples","DIR1","file1copy.txt");
+            Files.delete(fileToDelete);
+            Files.deleteIfExists(fileToDelete);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Paths","Examples","file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Paths","Examples","file1.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Paths","Examples","file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Paths","Examples","file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Paths","Examples","DIR1");
+//            copyFile = FileSystems.getDefault().getPath("Paths","Examples","DIR4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
         } catch (IOException  e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
