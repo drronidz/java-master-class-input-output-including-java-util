636e2aa5eb93ec260ff87f7c6181d386dbc6cf29
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-26 13:31:14 +0100

    issue done. closes #31

--- Paths/src/com/drronidz/Main.java
+++ Paths/src/com/drronidz/Main.java
@@ -14,39 +14,64 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-
-        System.out.println("******************************************************");
-
-//        Path filePath = FileSystems.getDefault().getPath("Paths","files","SubdirectoryFile.txt");
-        Path filePath = Paths.get("Paths","files","SubdirectoryFile.txt");
-        System.out.println(filePath);
-        printFile(filePath);
-        System.out.println("******************************************************");
 
 
-        filePath = Paths.get("");
-        System.out.println(filePath.toAbsolutePath());
-
-        System.out.println("******************************************************");
-
-        Path path2 = FileSystems.getDefault().getPath(".","Paths", "files", "..", "files", "SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath());
-
-
-    }
-
-    public static void printFile(Path path) {
-        try (BufferedReader fileReader = Files.newBufferedReader(path)){
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Paths","Examples","file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Paths","Examples","file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch (IOException  e) {
             e.printStackTrace();
         }
+
+
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//
+//        System.out.println("******************************************************");
+//
+////        Path filePath = FileSystems.getDefault().getPath("Paths","files","SubdirectoryFile.txt");
+//        Path filePath = Paths.get("Paths","files","SubdirectoryFile.txt");
+//        System.out.println(filePath);
+//        printFile(filePath);
+//        System.out.println("******************************************************");
+//
+//
+//        filePath = Paths.get("");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        System.out.println("******************************************************");
+//
+//        Path path2 = FileSystems.getDefault().getPath(".","Paths", "files", "..", "files", "SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//
+//        System.out.println("******************************************************");
+//
+//        Path path3 = FileSystems.getDefault().getPath("this_file_doesnt_exist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("C:\\","addf","whatever");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("Paths","files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//        System.out.println("Exists = " + Files.notExists(filePath));
+//
+//        System.out.println("Exists = " + Files.exists(path4));
+//        System.out.println("Exists = " + Files.notExists(path4));
+//
+//    }
+//
+//    public static void printFile(Path path) {
+//        try (BufferedReader fileReader = Files.newBufferedReader(path)){
+//            String line;
+//            while ((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
     }
 }
