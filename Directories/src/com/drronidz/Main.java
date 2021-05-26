2babebeceb8930abdf7f502dd96640d9d3df4d60
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-26 16:49:31 +0100

    issue done. closes #35

--- Directories/src/com/drronidz/Main.java
+++ Directories/src/com/drronidz/Main.java
@@ -5,11 +5,9 @@ package com.drronidz;/*
     CREATED ON : 2:30 PM
 */
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.Iterator;
 
 public class Main {
@@ -18,13 +16,42 @@ public class Main {
         DirectoryStream.Filter<Path> filter =
                 entry -> (Files.isRegularFile(entry));
 
-        Path directory = FileSystems.getDefault().getPath("Directories","FileTree\\DIR2");
+        //Path directory = FileSystems.getDefault().getPath("Directories","FileTree\\DIR2");
+        Path directory = FileSystems
+                .getDefault()
+                .getPath("Directories","FileTree"+ File.separator +"DIR2");
+
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for(Path file: contents) {
               System.out.println(file.getFileName());
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try {
+            Path tempFile = Files.createTempFile("myapp",".txt");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for(FileStore store : stores) {
+            System.out.println("Volume name/Drive letter = " + store);
+            System.out.println("file store = " + store.name());
+        }
+        System.out.println("########################################################");
+        Iterable<Path> rootPaths = FileSystems.getDefault().getRootDirectories();
+        for (Path path: rootPaths) {
+            System.out.println(path);
         }
     }
 }
